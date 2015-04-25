3d3ddb52545d53b63502e6ca94bb927689b9d546
Antonio <antonio.hdez93@gmail.com>
2015-04-25 11:10:17 -0500

    Hierarchical modeling for each face.

--- js/main.js
+++ js/main.js
@@ -17,17 +17,53 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
-var cube = new THREE.Mesh( geometry, material );
+//Root element
+var rubik = new THREE.Object3D();
+var whiteFace = new THREE.Object3D();
+var yellowFace = new THREE.Object3D();
+var greenFace = new THREE.Object3D();
+var blueFace = new THREE.Object3D();
+var redFace = new THREE.Object3D();
+var orangeFace = new THREE.Object3D();
 
-scene.add(cube);
+whiteFace.position.x = 1;
+yellowFace.position.x = -1;
+
+greenFace.position.y = 1;
+blueFace.position.y = -1;
+
+redFace.position.z = 1;
+orangeFace.position.z = -1;
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+var whiteMaterial = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
+var yellowMaterial = new THREE.MeshLambertMaterial( { color: 0xFFD500 } );
+var greenMaterial = new THREE.MeshLambertMaterial( { color: 0x009E60 } );
+var blueMaterial = new THREE.MeshLambertMaterial( { color: 0x0051BA } );
+var redMaterial = new THREE.MeshLambertMaterial( { color: 0xC41E3A } );
+var orangeMaterial = new THREE.MeshLambertMaterial( { color: 0xFF5800 } );
+
+whiteFace.add( new THREE.Mesh( geometry, whiteMaterial ) );
+yellowFace.add( new THREE.Mesh( geometry, yellowMaterial ) );
+greenFace.add( new THREE.Mesh( geometry, greenMaterial ) );
+blueFace.add( new THREE.Mesh( geometry, blueMaterial ) );
+redFace.add( new THREE.Mesh( geometry, redMaterial ) );
+orangeFace.add( new THREE.Mesh( geometry, orangeMaterial ) );
+
+rubik.add(whiteFace);
+rubik.add(yellowFace);
+rubik.add(greenFace);
+rubik.add(blueFace);
+rubik.add(redFace);
+rubik.add(orangeFace);
+
+scene.add(rubik);
 
 var ambientLight = new THREE.AmbientLight(0xFFFFFF);
 scene.add(ambientLight);
 
 camera.position.z = 10;
-
 
 var render = function () {
 	requestAnimationFrame( render );
