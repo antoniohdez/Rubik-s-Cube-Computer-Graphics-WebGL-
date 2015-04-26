e1aac35d48ba9f88101f06df331643ba2223d17f
Antonio <antonio.hdez93@gmail.com>
2015-04-25 22:12:51 -0500

    Refactoring

---
+++ js/drawCube.js
@@ -0,0 +1,67 @@
+var getMaterial = function (x, y, z){
+	var defaultColor = 0x000000;
+	var materials = [
+		new THREE.MeshLambertMaterial({ //color: 0x0051BA //Blue
+			color: defaultColor
+		}),
+		new THREE.MeshLambertMaterial({ //color: 0x009E60 //Green
+			color: defaultColor
+		}),
+		new THREE.MeshLambertMaterial({ //color: 0xFFFFFF //White
+			color: defaultColor
+		}),
+		new THREE.MeshLambertMaterial({ //color: 0xFFD500 //Yellow
+			color: defaultColor
+		}),
+		new THREE.MeshLambertMaterial({ //color: 0xC41E3A //Red
+			color: defaultColor
+		}),
+		new THREE.MeshLambertMaterial({ //color: 0xFF5800 //Orange
+			color: defaultColor
+		})
+	];
+	
+	if( x == 1 ){ //Blue
+		materials[0] = new THREE.MeshLambertMaterial({ color: 0x0051BA })
+	}else if( x == -1 ){ //Green
+		materials[1] = new THREE.MeshLambertMaterial({ color: 0x009E60 })
+	}
+
+	if( y == 1 ){ //White
+		materials[2] = new THREE.MeshLambertMaterial({ color: 0xFFFFFF })
+	}else if( y == -1 ){ //Yellow
+		materials[3] = new THREE.MeshLambertMaterial({ color: 0xFFD500 })
+	}
+
+	if( z == 1 ){ //Red
+		materials[4] = new THREE.MeshLambertMaterial({ color: 0xC41E3A })
+	}else if( z == -1 ){ //Orange
+		materials[5] = new THREE.MeshLambertMaterial({ color: 0xFF5800 })
+	}
+
+	return materials;
+}
+
+var drawCube = function (rubik){
+	//var i = 1;
+	for(var x = -1; x <= 1; x++ ){
+		for(var y = -1; y <= 1; y++ ){
+			for(var z = -1; z <= 1; z++ ){
+				//console.log( "Line " + (i++) + " " + "X: " + x + " Y: " + y + " Z: " + z );
+				
+				materials = getMaterial(x, y, z);
+
+				cube = new THREE.Mesh(
+				    new THREE.CubeGeometry(0.5, 0.5, 0.5, 1, 1, 1),
+				    new THREE.MeshFaceMaterial( materials ));
+				cube.position.x = x;
+				cube.position.y = y;
+				cube.position.z = z;
+
+				rubik.add(cube);
+
+			}
+		}
+	}
+	return rubik;
+}
--- js/main.js
+++ js/main.js
@@ -1,66 +1,3 @@
-var getMaterial = function (x, y, z){
-	var defaultColor = 0x000000;
-	var materials = [
-		new THREE.MeshLambertMaterial({
-			//color: 0x0051BA //Blue
-			color: defaultColor
-		}),
-		new THREE.MeshLambertMaterial({
-			//color: 0x009E60 //Green
-			color: defaultColor
-		}),
-		new THREE.MeshLambertMaterial({
-			//color: 0xFFFFFF //White
-			color: defaultColor
-		}),
-		new THREE.MeshLambertMaterial({
-			//color: 0xFFD500 //Yellow
-			color: defaultColor
-		}),
-		new THREE.MeshLambertMaterial({
-			//color: 0xC41E3A //Red
-			color: defaultColor
-		}),
-		new THREE.MeshLambertMaterial({
-			//color: 0xFF5800 //Orange
-			color: defaultColor
-		})
-	];
-	
-	if( x == 1 ){
-		//Blue
-		materials[0] = new THREE.MeshLambertMaterial({ color: 0x0051BA })
-	}else if( x == 0 ){
-		//No color needed
-	}else if( x == -1 ){
-		//Green
-		materials[1] = new THREE.MeshLambertMaterial({ color: 0x009E60 })
-	}
-
-	if( y == 1 ){
-		//White
-		materials[2] = new THREE.MeshLambertMaterial({ color: 0xFFFFFF })
-	}else if( y == 0 ){
-		//No color needed
-	}else if( y == -1 ){
-		//Yellow
-		materials[3] = new THREE.MeshLambertMaterial({ color: 0xFFD500 })
-	}
-
-	if( z == 1 ){
-		//Red
-		materials[4] = new THREE.MeshLambertMaterial({ color: 0xC41E3A })
-	}else if( z == 0 ){
-		//No color needed
-	}else if( z == -1 ){
-		//Orange
-		materials[5] = new THREE.MeshLambertMaterial({ color: 0xFF5800 })
-	}
-	return materials;
-	
-}
-
-
 var scene = new THREE.Scene();
 // (angle, aspect, near, far)
 /*
@@ -89,67 +26,17 @@ document.body.appendChild( renderer.domElement );
 var rubik = new THREE.Object3D();
 
 //Faces
-var whiteFace = new THREE.Object3D();
-var yellowFace = new THREE.Object3D();
-var greenFace = new THREE.Object3D();
-var blueFace = new THREE.Object3D();
-var redFace = new THREE.Object3D();
-var orangeFace = new THREE.Object3D();
-
-blueFace.position.x = 1;
-greenFace.position.x = -1;
-
-whiteFace.position.y = 1;
-yellowFace.position.y = -1;
-
-redFace.position.z = 1;
-orangeFace.position.z = -1;
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-
-var whiteMaterial = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
-var yellowMaterial = new THREE.MeshLambertMaterial( { color: 0xFFD500 } );
-var greenMaterial = new THREE.MeshLambertMaterial( { color: 0x009E60 } );
-var blueMaterial = new THREE.MeshLambertMaterial( { color: 0x0051BA } );
-var redMaterial = new THREE.MeshLambertMaterial( { color: 0xC41E3A } );
-var orangeMaterial = new THREE.MeshLambertMaterial( { color: 0xFF5800 } );
-
-whiteFace.add( new THREE.Mesh( geometry, whiteMaterial ) );
-yellowFace.add( new THREE.Mesh( geometry, yellowMaterial ) );
-greenFace.add( new THREE.Mesh( geometry, greenMaterial ) );
-blueFace.add( new THREE.Mesh( geometry, blueMaterial ) );
-redFace.add( new THREE.Mesh( geometry, redMaterial ) );
-orangeFace.add( new THREE.Mesh( geometry, orangeMaterial ) );
-
-var i = 1;
-for(var x = -1; x <= 1; x++ ){
-	for(var y = -1; y <= 1; y++ ){
-		for(var z = -1; z <= 1; z++ ){
-			console.log( "Line " + (i++) + " " + "X: " + x + " Y: " + y + " Z: " + z );
-			
-			materials = getMaterial(x, y, z);
-
-			dice = new THREE.Mesh(
-			    new THREE.CubeGeometry(0.9, 0.9, 0.9, 1, 1, 1),
-			    new THREE.MeshFaceMaterial( materials ));
-			dice.position.x = x;
-			dice.position.y = y;
-			dice.position.z = z;
-
-			rubik.add(dice);
-
-		}
-	}
+var sides = {
+	"white"  : new THREE.Object3D(),
+	"yellow" : new THREE.Object3D(),
+	"green"  : new THREE.Object3D(),
+	"blue"   : new THREE.Object3D(),
+	"red"    : new THREE.Object3D(),
+	"orange" : new THREE.Object3D()
 }
 
-/*
-rubik.add(whiteFace);
-rubik.add(yellowFace);
-rubik.add(greenFace);
-rubik.add(blueFace);
-rubik.add(redFace);
-rubik.add(orangeFace);
-*/
+
+rubik = drawCube(rubik);
 
 scene.add(rubik);
 
